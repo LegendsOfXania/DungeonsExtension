7f7d5286193acb339657678f4b53bf0e99ce4fc0
LegendsOfXania <legendsofxania@gmail.com>
2025-07-18 14:49:56 +0200

    Add @Help annotation in SimpleDungeonDefinition

--- src/main/kotlin/fr/xania/dungeons/entries/static/SimpleDungeonDefinitionStaticEntry.kt
+++ src/main/kotlin/fr/xania/dungeons/entries/static/SimpleDungeonDefinitionStaticEntry.kt
@@ -3,6 +3,7 @@ package fr.xania.dungeons.entries.static
 import com.typewritermc.core.books.pages.Colors
 import com.typewritermc.core.entries.Ref
 import com.typewritermc.core.extension.annotations.Entry
+import com.typewritermc.core.extension.annotations.Help
 import com.typewritermc.engine.paper.entry.StaticEntry
 
 @Entry(
@@ -14,5 +15,6 @@ import com.typewritermc.engine.paper.entry.StaticEntry
 class SimpleDungeonDefinitionStaticEntry (
     override val id: String = "",
     override val name: String = "",
-    val rooms: List<Ref<SimpleRoomDefinitionArtifactEntry>> = emptyList()
+    @Help("The first room of the dungeon.")
+    val room: List<Ref<SimpleRoomDefinitionArtifactEntry>> = emptyList()
 ) : StaticEntry
