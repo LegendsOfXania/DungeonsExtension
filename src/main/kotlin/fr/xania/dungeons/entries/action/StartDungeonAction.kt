adf1f3f4c780b0fb8f5b7504a190b9c4a9087512
LegendsOfXania <legendsofxania@gmail.com>
2025-07-21 20:57:59 +0200

    Start working on placement

---
+++ src/main/kotlin/fr/xania/dungeons/entries/action/StartDungeonAction.kt
@@ -0,0 +1,41 @@
+package fr.xania.dungeons.entries.action
+
+import com.typewritermc.core.books.pages.Colors
+import com.typewritermc.core.entries.Ref
+import com.typewritermc.core.entries.emptyRef
+import com.typewritermc.core.extension.annotations.Entry
+import com.typewritermc.core.extension.annotations.Help
+import com.typewritermc.engine.paper.entry.Criteria
+import com.typewritermc.engine.paper.entry.Modifier
+import com.typewritermc.engine.paper.entry.TriggerableEntry
+import com.typewritermc.engine.paper.entry.entries.ActionEntry
+import com.typewritermc.engine.paper.entry.entries.ActionTrigger
+import fr.xania.dungeons.entries.manifest.DungeonDefinition
+
+@Entry(
+    "start_dungeon_action",
+    "Start a dungeon for a group",
+    Colors.RED,
+    "carbon:build-image"
+)
+/**
+ * The `StartDungeonAction` entry is used to start a dungeon for a group.
+ *
+ * ## How could this be used?
+ *
+ * This could be used to start a dungeon for a player, initializing the dungeon's state
+ * and preparing him for the adventure.
+ */
+class StartDungeonAction (
+    override val id: String = "",
+    override val name: String = "",
+    override val criteria: List<Criteria> = emptyList(),
+    override val modifiers: List<Modifier> = emptyList(),
+    override val triggers: List<Ref<TriggerableEntry>> = emptyList(),
+    @Help("The dungeon to start.")
+    val dungeon: Ref<DungeonDefinition> = emptyRef()
+) : ActionEntry {
+    override fun ActionTrigger.execute() {
+        // Do something with the player
+    }
+}
