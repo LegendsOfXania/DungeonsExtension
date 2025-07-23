91c8a06bcf00544be872ae621dd8b19e53ace8db
LegendsOfXania <legendsofxania@gmail.com>
2025-07-23 19:27:40 +0200

    Fix Entries renamming

--- src/main/kotlin/fr/xania/dungeons/entries/manifest/DungeonInstanceManifest.kt
+++ src/main/kotlin/fr/xania/dungeons/entries/manifest/DungeonInstanceManifest.kt
@@ -9,22 +9,22 @@ import com.typewritermc.core.extension.annotations.Tags
 import com.typewritermc.engine.paper.entry.ManifestEntry
 
 @Entry(
-    "dungeon_definition",
-    "The definition of a dungeon",
+    "dungeon_Instance",
+    "The Instance of a dungeon",
      Colors.ORANGE,
     "tabler:building"
 )
-@Tags("dungeon_defition")
+@Tags("dungeon_instance")
 /**
- * The `DungeonDefinition` entry is used to define dungeon.
+ * The `DungeonInstance` entry is used to define dungeon.
  *
  * ## How could this be used?
  *
  * This could be used to define a dungeon and the first room(s) of the dungeon.
  */
-class DungeonDefinition (
+class DungeonInstance (
     override val id: String = "",
     override val name: String = "",
     @Help("The first room of the dungeon.")
-    val children: Ref<RoomDefinition> = emptyRef()
+    val children: Ref<RoomInstance> = emptyRef(),
 ) : ManifestEntry
