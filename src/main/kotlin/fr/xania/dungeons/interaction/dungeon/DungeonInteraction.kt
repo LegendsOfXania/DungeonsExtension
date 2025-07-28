10f964d83e08ac3099bb5c2d33ef2dfcd726d9f1
LegendsOfXania <legendsofxania@gmail.com>
2025-07-28 17:16:05 +0200

    Remove the test in DungeonInteraction

--- src/main/kotlin/fr/xania/dungeons/interaction/dungeon/DungeonInteraction.kt
+++ src/main/kotlin/fr/xania/dungeons/interaction/dungeon/DungeonInteraction.kt
@@ -1,21 +1,15 @@
 package fr.xania.dungeons.interaction.dungeon
 
-import com.typewritermc.core.entries.Query
 import com.typewritermc.core.entries.Ref
 import com.typewritermc.core.entries.emptyRef
 import com.typewritermc.core.interaction.Interaction
 import com.typewritermc.core.interaction.InteractionContext
 import com.typewritermc.core.utils.ok
-import com.typewritermc.engine.paper.entry.AssetManager
 import com.typewritermc.engine.paper.entry.entries.EventTrigger
 import com.typewritermc.engine.paper.entry.triggerFor
-import com.typewritermc.engine.paper.utils.server
 import fr.xania.dungeons.entries.manifest.DungeonInstance
 import fr.xania.dungeons.manager.DungeonManager
-import fr.xania.dungeons.manager.WorldManager
 import org.bukkit.entity.Player
-import org.koin.java.KoinJavaComponent
-import java.io.File
 import java.time.Duration
 
 class DungeonInteraction(
@@ -28,26 +22,6 @@ class DungeonInteraction(
     override suspend fun initialize(): Result<Unit> {
 
         DungeonManager.initialize(dungeon)
-
-        /* An ugly test
-        val entry = Query.findById<DungeonInstance>(dungeon.id) ?: return Result.failure(
-            IllegalArgumentException("DungeonInstance with id '${dungeon.id}' not found.")
-        )
-
-        val firstRoom = entry.children.entry ?: return Result.failure(
-            IllegalArgumentException("DungeonInstance with id '${dungeon.id}' has no first room defined.")
-        )
-        val firstRoomArtifact = firstRoom.artifact.entry!!
-        val assetManager = KoinJavaComponent.get<AssetManager>(AssetManager::class.java)
-        val assets = assetManager.fetchBinaryAsset(firstRoomArtifact)!!
-
-        val tempFile = File.createTempFile("plugins/Typewriter/.temp/${firstRoomArtifact.artifactId}", ".nbt")
-        tempFile.writeBytes(assets)
-
-        val structure = server.structureManager.loadStructure(tempFile)
-
-        tempFile.delete()
-*/
 
         player.sendMessage("Starting interaction!")
 
