33f4b7077bf994040c4ca835ee551cae1db7dd5f
LegendsOfXania <legendsofxania@gmail.com>
2025-08-21 21:20:23 +0200

    Remove unused function

--- src/main/kotlin/fr/legendsofxania/dungeons/data/DungeonRoomBounds.kt
+++ src/main/kotlin/fr/legendsofxania/dungeons/data/DungeonRoomBounds.kt
@@ -8,11 +8,4 @@ data class DungeonRoomBounds(
     val room: Ref<RoomInstance>,
     val minLoc: Location,
     val maxLoc: Location
-) {
-    fun contains(location: Location): Boolean {
-        return location.world == minLoc.world &&
-                location.x >= minLoc.x && location.x <= maxLoc.x &&
-                location.y >= minLoc.y && location.y <= maxLoc.y &&
-                location.z >= minLoc.z && location.z <= maxLoc.z
-    }
-}
+)
