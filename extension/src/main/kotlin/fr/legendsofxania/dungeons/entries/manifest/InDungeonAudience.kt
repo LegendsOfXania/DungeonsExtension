3bae1523784c0aca58c3beb0ce9d5952e8ecad11
LegendsOfXania <legendsofxania@gmail.com>
2025-09-11 19:01:07 +0200

    Add AudienceEntries

---
+++ extension/src/main/kotlin/fr/legendsofxania/dungeons/entries/manifest/InDungeonAudience.kt
@@ -0,0 +1,48 @@
+package fr.legendsofxania.dungeons.entries.manifest
+
+import com.typewritermc.core.books.pages.Colors
+import com.typewritermc.core.entries.Ref
+import com.typewritermc.core.entries.ref
+import com.typewritermc.core.extension.annotations.Entry
+import com.typewritermc.engine.paper.entry.entries.AudienceEntry
+import com.typewritermc.engine.paper.entry.entries.AudienceFilter
+import com.typewritermc.engine.paper.entry.entries.AudienceFilterEntry
+import com.typewritermc.engine.paper.entry.entries.Invertible
+import fr.legendsofxania.dungeons.managers.PlayerManager.getDungeon
+import org.bukkit.entity.Player
+import java.util.Optional
+
+@Entry("in_dungeon_audience", "Return if a player is in a dungeon or not.", Colors.GREEN, "material-symbols:chat-rounded")
+class InDungeonAudience(
+    override val id: String = "",
+    override val name: String = "",
+    override val children: List<Ref<out AudienceEntry>> = emptyList(),
+    val dungeon: Optional<Ref<DungeonDefinition>> = Optional.empty(),
+    override val inverted: Boolean = false,
+) : AudienceFilterEntry, Invertible {
+    override suspend fun display(): AudienceFilter = InDungeonAudienceFilter(dungeon, inverted, ref())
+}
+
+class InDungeonAudienceFilter(
+    private val dungeon: Optional<Ref<DungeonDefinition>>,
+    private val inverted: Boolean,
+    ref: Ref<out AudienceFilterEntry>
+) : AudienceFilter(ref) {
+
+    override fun filter(player: Player): Boolean {
+        val playerDungeon = player.getDungeon()
+
+        val isInTargetDungeon = if (dungeon.isPresent) {
+            val targetDungeon = dungeon.get()
+            playerDungeon?.definition == targetDungeon
+        } else {
+            playerDungeon != null
+        }
+
+        return if (inverted) {
+            !isInTargetDungeon
+        } else {
+            isInTargetDungeon
+        }
+    }
+}
---
+++ extension/src/main/kotlin/fr/legendsofxania/dungeons/entries/manifest/InRoomAudience.kt
@@ -0,0 +1,48 @@
+package fr.legendsofxania.dungeons.entries.manifest
+
+import com.typewritermc.core.books.pages.Colors
+import com.typewritermc.core.entries.Ref
+import com.typewritermc.core.entries.ref
+import com.typewritermc.core.extension.annotations.Entry
+import com.typewritermc.engine.paper.entry.entries.AudienceEntry
+import com.typewritermc.engine.paper.entry.entries.AudienceFilter
+import com.typewritermc.engine.paper.entry.entries.AudienceFilterEntry
+import com.typewritermc.engine.paper.entry.entries.Invertible
+import fr.legendsofxania.dungeons.managers.PlayerManager.getCurrentRoom
+import org.bukkit.entity.Player
+import java.util.Optional
+
+@Entry("in_room_audience", "Return if a player is in a room or not.", Colors.GREEN, "material-symbols:chat-rounded")
+class InRoomAudience(
+    override val id: String = "",
+    override val name: String = "",
+    override val children: List<Ref<out AudienceEntry>> = emptyList(),
+    val room: Optional<Ref<RoomDefinition>> = Optional.empty(),
+    override val inverted: Boolean = false,
+) : AudienceFilterEntry, Invertible {
+    override suspend fun display(): AudienceFilter = InRoomAudienceFilter(room, inverted, ref())
+}
+
+class InRoomAudienceFilter(
+    private val room: Optional<Ref<RoomDefinition>>,
+    private val inverted: Boolean,
+    ref: Ref<out AudienceFilterEntry>
+) : AudienceFilter(ref) {
+
+    override fun filter(player: Player): Boolean {
+        val playerRoom = player.getCurrentRoom()
+
+        val isInTargetRoom = if (room.isPresent) {
+            val targetRoom = room.get()
+            playerRoom?.definition == targetRoom
+        } else {
+            playerRoom != null
+        }
+
+        return if (inverted) {
+            !isInTargetRoom
+        } else {
+            isInTargetRoom
+        }
+    }
+}
