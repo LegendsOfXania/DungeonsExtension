c6b992116e2f05336d2e75e63ec743cdc3be4137
LegendsOfXania <legendsofxania@gmail.com>
2025-09-29 15:42:20 +0200

    Add %typewriter_in_dungeon% placeholder

---
+++ extension/src/main/kotlin/fr/legendsofxania/dungeon/utils/DungeonPlaceholders.kt
@@ -0,0 +1,22 @@
+package fr.legendsofxania.dungeon.utils
+
+import com.typewritermc.core.extension.annotations.Singleton
+import com.typewritermc.engine.paper.extensions.placeholderapi.PlaceholderHandler
+import fr.legendsofxania.dungeon.managers.PlayerManager
+import org.bukkit.entity.Player
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+@Singleton
+class DungeonPlaceholders : PlaceholderHandler, KoinComponent {
+    private val playerManager: PlayerManager by inject()
+
+    override fun onPlaceholderRequest(player: Player?, params: String): String? {
+        if (player == null) return null
+        if (params != "in_dungeon") return null
+
+        return if (playerManager.isInDungeon(player)) "1" else "0"
+
+
+    }
+}
